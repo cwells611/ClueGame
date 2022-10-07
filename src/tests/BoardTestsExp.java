a095e9caad77f5da6478517d8885458bc12d9c40
Caden <cwells@mines.edu>
2022-10-06 18:33:20 -0600

    Began writing @BeforeEach method for testing

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -4,8 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class BoardTestsExp {
+import experiment.TestBoardCell;
 
+class BoardTestsExp {
+	//Board attribute to test behavior with 
+	TestBoard board; 
+	
+	@BeforeEach 
+	//Before each tests runs, creates new board object with will create a new adjacency lust
+	public void creatBoard() {
+		board = new TestBoard(); 
+	}
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
