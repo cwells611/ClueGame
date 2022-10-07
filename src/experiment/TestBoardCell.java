9e40c4aa273e353fe47177ba7728de7f0fd867b7
Caden <cwells@mines.edu>
2022-10-06 18:17:10 -0600

    Created skeleton of TestBoardCell() class in experiment package

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,7 +1,39 @@
 package experiment;
 
+import java.util.*;
+
 public class TestBoardCell {
-	public static void main(String[] args) {
-		System.out.println("Test");
+	//attributes that define the row and column of the current board cell
+	private int row;
+	private int col;
+	//set to contain this cells adjacency list 
+	private Set<TestBoardCell> adjList;
+	//boolean that determines says if a cell is occupied by another player
+	private boolean occupiedSpace;
+	
+	public TestBoardCell(int row, int col) {
+		this.row = row;
+		this.col = col; 
 	}
+	
+	//setter setter to add a cell to this cells adjacency list
+	public void addAdjacency(TestBoardCell cell) {
+		
+	}
+	
+	//getter that returns adjacency list for current sell
+	public Set<TestBoardCell> getAdjList() {
+		return this.adjList;
+	}
+	
+	//setter to indicate if a cell is occupied by another player 
+	public void setOccupied(boolean occupied) {
+		
+	}
+	
+	//getter that returns boolean based on if a cell is occupied by another player 
+	public boolean getOccupied() {
+		return this.occupiedSpace;
+	}
+	
 }
