ffae15eb016213b1291a66934e120bf5e84255a6
zcrennen <98796321+zcrennen@users.noreply.github.com>
2022-10-07 15:03:03 -0600

    Added adjacetTopLeft

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -2,6 +2,11 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import experiment.TestBoardCell;
@@ -12,13 +17,21 @@ class BoardTestsExp {
 	
 	@BeforeEach 
 	//Before each tests runs, creates new board object with will create a new adjacency lust
-	public void creatBoard() {
+	public void createBoard() {
 		board = new TestBoard(); 
 	}
 	
 	@Test
 	void test() {
 		fail("Not yet implemented");
+	}
+	
+	public void adjacentTopLeft() {
+		TestBoardCell cell = board.getCell(0,0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(0,1)));
+		Assert.assertTrue(testList.contains(board.getCell(1,0)));
+		Assert.assertEquals(2, testList.size());
 	}
 
 }
