fa9dfef880260b9109583983171ccb271f162884
zcrennen <98796321+zcrennen@users.noreply.github.com>
2022-10-09 20:37:25 -0600

    Added comments to FileInitTest

--- src/tests/FileInitTest.java
+++ src/tests/FileInitTest.java
@@ -1,5 +1,11 @@
 package tests;
 
 public class FileInitTest {
-
+//	ensure your layout and setup files are loaded correctly (correct number of rooms, test several entries including first and last in file)
+//	ensure the correct number of rows/columns have been read
+//	verify at least one doorway in each direction. Also verify cells that don't contain doorways return false for isDoorway().
+//	check that the correct number of doors have been loaded.
+//	check some of the cells to ensure they have the correct initial
+//	check that rooms have the proper center cell and label cell.
+//	implement @BeforeEach method or @BeforeAll
 }
