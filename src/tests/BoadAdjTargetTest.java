20b8a415ad6ec5c3893a6c9349fd869d29b14f3c
zcrennen <98796321+zcrennen@users.noreply.github.com>
2022-10-14 17:34:38 -0600

    Filled out adjacency tests
    Worked via codeTogether

--- src/tests/BoadAdjTargetTest.java
+++ src/tests/BoadAdjTargetTest.java
@@ -6,6 +6,8 @@ import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 import clueGame.Board;
 import clueGame.BoardCell;
 import clueGame.DoorDirection;
@@ -38,31 +40,58 @@ public class BoadAdjTargetTest {
 	//@Test
 	public void testWalkwayAdjacencies(){
 		//test spot (4,2)
+		Set<BoardCell> adjList = theBoard.getAdjList(4,2);
+		assertEquals(4, adjList.size());
+		assertTrue(adjList.contains(theBoard.getCell(4, 1)));
+		assertTrue(adjList.contains(theBoard.getCell(4, 3)));
+		assertTrue(adjList.contains(theBoard.getCell(3, 2)));
+		assertTrue(adjList.contains(theBoard.getCell(5, 2)));
 	}
 //	Locations within rooms not center (should have empty adjacency list)
 	//@Test
 	public void testNotCenterRoomAdjacencies() {
 		//test spot (0,10)
+		Set<BoardCell> adjList = theBoard.getAdjList(0, 10); 
+		assertEquals(0, adjList.size()); 
 	}
 //	Locations that are at each edge of the board
 	//@Test
 	public void testEdgeAdjacencies(){
-		//test spot (1,24)
+		//test spot (10,24)
+		Set<BoardCell> adjList = theBoard.getAdjList(1, 24); 
+		assertEquals(3, adjList.size()); 
+		assertTrue(adjList.contains(theBoard.getCell(7, 23))); 
+		assertTrue(adjList.contains(theBoard.getCell(11, 24))); 
+		assertTrue(adjList.contains(theBoard.getCell(10, 23))); 
 	}
 //	Locations that are beside a room cell that is not a doorway	
 	//@Test
 	public void testBesideRoomAdjacency() {
 		//test spot (24, 8)
+		Set<BoardCell> adjList = theBoard.getAdjList(24, 8); 
+		assertEquals(2, adjList.size());
+		assertTrue(adjList.contains(theBoard.getCell(24, 7))); 
+		assertTrue(adjList.contains(theBoard.getCell(24, 9)));
 	}
 //	Locations that are doorways
 	//@Test
 	public void testDoorwayAdjacency() {
 		//test spot (7,5)
+		Set<BoardCell> adjList = theBoard.getAdjList(7, 5); 
+		assertEquals(4, adjList.size()); 
+		assertTrue(adjList.contains(theBoard.getCell(10, 2)));
+		assertTrue(adjList.contains(theBoard.getCell(6, 5))); 
+ 		assertTrue(adjList.contains(theBoard.getCell(8, 5))); 
+ 		assertTrue(adjList.contains(theBoard.getCell(7, 6))); 
 	}
 //	Locations that are connected by secret passage	
 	//@Test
 	public void testSecretPassage() {
 		//test spot (21, 23)
+		Set<BoardCell> adjList = theBoard.getAdjList(21, 23); 
+		assertEquals(2, adjList.size());
+		assertTrue(adjList.contains(theBoard.getCell(20, 22)));
+		assertTrue(adjList.contains(theBoard.getCell(3, 8)));
 	}
 	
 /*
--- src/tests/BoardAdjTargetTest306.java
+++ src/tests/BoardAdjTargetTest306.java
@@ -35,7 +35,6 @@ public class BoardAdjTargetTest306 {
 		// we want to test a couple of different rooms.
 		// First, the study that only has a single door but a secret room
 		Set<BoardCell> testList = board.getAdjList(2, 2);
-		System.out.println(testList.size());
 		assertEquals(2, testList.size());
 		assertTrue(testList.contains(board.getCell(4, 6)));
 		assertTrue(testList.contains(board.getCell(20, 19)));
