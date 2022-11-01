340ca492bac50300355f911311b46e056c612492
Caden <cwells@mines.edu>
2022-11-01 13:38:47 -0600

    updated ClueSetup.txt and added player info

--- src/tests/PlayerAndCardTests.java
+++ src/tests/PlayerAndCardTests.java
@@ -2,13 +2,30 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class PlayerAndCardTests {
+import clueGame.Board;
 
+class PlayerAndCardTests {
+	//constant variables to hold board size 
+	public static final int ROWS = 25; 
+	public static final int COLS = 25; 
+	private static Board theBoard;
+
+	@BeforeAll
+	public static void setUp() {
+		//since we are only using one instance of this board 
+		theBoard = Board.getInstance(); 
+		//has the board read in the config files and setup board based on files 
+		theBoard.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		//loads both files even though we are only using one instance of board 
+		theBoard.initialize();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testNumPlayers() {
+		
 	}
 
 }
