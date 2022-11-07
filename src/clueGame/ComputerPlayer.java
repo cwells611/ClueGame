456f416cdd6adce7401d5cd4bcc9d844510d7398
Caden <cwells@mines.edu>
2022-11-06 20:38:59 -0700

    wrote out method stubs for new methods in ComputerPlayer and set up test file for the computer AI tests

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -1,10 +1,24 @@
 package clueGame;
 
-public class ComputerPlayer extends Player{ 
+import java.util.Set;
+
+public class ComputerPlayer extends Player{
 
 	public ComputerPlayer(String name, String color, int startRow, int startCol, String type) {
 		super(name, color, startRow, startCol, type);
 	}
-
+	
+	//even though we are creating a suggestion, since the solution class already utilizes creation 
+	//of a room, weapon, and person card, we will use this class for our suggestion
+	public Solution createSuggestion(Room currentRoom) {
+		
+		return null; 
+	}
+	
+	//the computer player needs to be able to select its next target, so we will return that target given 
+	//the set of boardcells that are available targets 
+	public BoardCell selectTarget(Set<BoardCell> targets) {
+		return null;
+	}
 
 }
--- src/tests/ComputerAITest.java
+++ src/tests/ComputerAITest.java
@@ -1,5 +1,43 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import clueGame.Board;
+import clueGame.Card;
+import clueGame.CardType;
+import clueGame.ComputerPlayer;
+import clueGame.Player;
+import clueGame.Solution;
+
 public class ComputerAITest {
+	//constant variables to hold board size 
+	public static final int ROWS = 25; 
+	public static final int COLS = 25; 
+	private static Board theBoard;
+
+	@BeforeAll
+	public static void setUp() {
+		//since we are only using one instance of this board 
+		theBoard = Board.getInstance(); 
+		//has the board read in the config files and setup board based on files 
+		theBoard.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		//loads both files even though we are only using one instance of board 
+	}
+
+	//necessary to initialize before each, as deck size must be different between tests
+	@BeforeEach
+	public void initialize() {
+		theBoard.initialize();
+	}
 
 }
