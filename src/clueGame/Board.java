09cd8f4252824a3c6b64d725f83bc41a4694bc29
Caden <cwells@mines.edu>
2022-11-19 18:47:39 -0700

    made invalid target frame more informative

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -687,7 +687,7 @@ public class Board {
 			}
 
 			if(!clickedOnTarget) {
-				JOptionPane.showMessageDialog(BoardPanel.getTheBoardPanel(), "Please select a valid target.");
+				JOptionPane.showMessageDialog(BoardPanel.getTheBoardPanel(), "Please select a valid target.", "Invalid Target", JOptionPane.INFORMATION_MESSAGE);
 				return;
 			}else {
 				//moving the player
