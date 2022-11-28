fce74c2ce41095b2d4680104e8c545af1c8b1cfe
zcrennen <98796321+zcrennen@users.noreply.github.com>
2022-11-28 15:20:19 -0700

    Created rooms people and weapons arraylists
    Added initialization to them
    
    Also created createComboBox

--- src/clueGame/SuggestionAccusationPanel.java
+++ src/clueGame/SuggestionAccusationPanel.java
@@ -21,14 +21,45 @@ public class SuggestionAccusationPanel extends JDialog {
 	private JLabel currentRoomLabel;
 	private JLabel personLabel;
 	private JLabel weaponLabel;
-	private JComboBox roomOption;
-	private JComboBox personOption;
-	private JComboBox weaponOption;
+	//both a combo box and text field exist for the room
+		//combo box for an accusation
+		//text field for a suggestion
+	private JComboBox<String> roomOption;
+	private JTextField roomTextField;
+	private JComboBox<String> personOption;
+	private JComboBox<String> weaponOption;
 	private JButton submitButton;
 	private JButton cancelButton;
+	private ArrayList<String> rooms;
+	private ArrayList<String> people;
+	private ArrayList<String> weapons;
 	
 	
 	public SuggestionAccusationPanel(boolean isSuggestion) {
+		//creating array lists of strings corresponding to each card type
+		rooms = new ArrayList<String>();
+		people = new ArrayList<String>();
+		weapons = new ArrayList<String>();
+		//defining all of the cards to be the deck, plus the 3 cards in the solution
+		ArrayList<Card> allCards = Board.getInstance().getDeck();
+		allCards.add(Board.getInstance().getTheAnswer().getRoom());
+		allCards.add(Board.getInstance().getTheAnswer().getPerson());
+		allCards.add(Board.getInstance().getTheAnswer().getWeapon());
+		//iterating
+		for(Card card : allCards) {
+			switch(card.getType()) {
+			case ROOM:
+				rooms.add(card.getName());
+				break;
+			case PERSON:
+				people.add(card.getName());
+				break;
+			case WEAPON:
+				weapons.add(card.getName());
+				break;
+			}
+		}
+		
 		//setting the title depending on if it is a suggestion or accusation
 		if(isSuggestion) {
 			setTitle("Make a suggestion");
@@ -37,9 +68,26 @@ public class SuggestionAccusationPanel extends JDialog {
 		}
 		setSize(300,200);
 		setLayout(new GridLayout(4,2));
-		
+		currentRoomLabel = new JLabel("Current room");
+		if(isSuggestion) {
+			roomTextField = new JTextField("TEMP");
+		}else{
+			//getting the room options
+			
+			//roomOption = new JComboBox;
+		}
+		personLabel = new JLabel("Person");
+		weaponLabel = new JLabel("Weapon");
 		
 	}
 	
+	
+	private JComboBox<String> createComboBox(ArrayList<String> options){
+		JComboBox<String> combo = new JComboBox<String>();
+		for(String option : options) {
+			combo.addItem(option);
+		}
+		return combo;
+	}
 	
 }
