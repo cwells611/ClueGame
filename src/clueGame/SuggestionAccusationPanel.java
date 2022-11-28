d62f55dc4e55f845dd5d5d715f512ce1eac4bf74
zcrennen <98796321+zcrennen@users.noreply.github.com>
2022-11-28 14:53:04 -0700

    created instance variables and started constructor

--- src/clueGame/SuggestionAccusationPanel.java
+++ src/clueGame/SuggestionAccusationPanel.java
@@ -1,5 +1,45 @@
 package clueGame;
 
-public class SuggestionAccusationPanel {
+import javax.swing.JDialog;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.SwingConstants;
+
+public class SuggestionAccusationPanel extends JDialog {
+	private JLabel currentRoomLabel;
+	private JLabel personLabel;
+	private JLabel weaponLabel;
+	private JComboBox roomOption;
+	private JComboBox personOption;
+	private JComboBox weaponOption;
+	private JButton submitButton;
+	private JButton cancelButton;
+	
+	
+	public SuggestionAccusationPanel(boolean isSuggestion) {
+		//setting the title depending on if it is a suggestion or accusation
+		if(isSuggestion) {
+			setTitle("Make a suggestion");
+		}else {
+			setTitle("Make an accusation");
+		}
+		setSize(300,200);
+		setLayout(new GridLayout(4,2));
+		
+		
+	}
+	
+	
 }
